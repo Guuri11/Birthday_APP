5c4eb3e4508a2b8e33aea426ce5a81c436effbbb
Guuri11 <sergio.gurillo11@gmail.com>
2020-10-12 17:21:57 +0200

    firebase template added

---
+++ src/utils/firebase_template.js
@@ -0,0 +1,14 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
+
+// Initialize Firebase
+//export default firebase.initializeApp(firebaseConfig);
